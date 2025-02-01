7684a4e64cf7298ea11c954d108bf0a9edf01a22
sawarame <yasushi@sawara.me>
2025-02-01 15:02:50 +0900

    レイヤー数追加（5->6)

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/sawarame/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/sawarame/config.h
@@ -35,8 +35,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 2
-
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
 #define TAPPING_TERM 150
