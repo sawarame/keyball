3b3ae3ce94887b7fd5edef6732bdc816ce6e7beb
sawarame <yasushi@sawara.me>
2025-02-07 22:09:44 +0900

    オートマウス修正

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/sawarame/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/sawarame/config.h
@@ -37,7 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 2
-#define AUTO_MOUSE_TIME 300
+#define AUTO_MOUSE_TIME 200
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5 // レイヤー数
 
