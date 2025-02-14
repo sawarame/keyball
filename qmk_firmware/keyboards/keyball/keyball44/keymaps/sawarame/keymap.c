b29234c1b4e3186ceb3a4f369d17c16a5eaf091c
sawarame <yasushi@sawara.me>
2025-02-15 02:28:44 +0900

    typo修正

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/sawarame/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/sawarame/keymap.c
@@ -63,7 +63,7 @@ void pointing_device_init_user(void) {
 layer_state_t layer_state_set_user(layer_state_t state) {
   // Auto enable scroll mode when the highest layer is 1
   keyball_set_scroll_mode(get_highest_layer(state) == 1);
-  eturn state;
+  return state;
 }
 
 #ifdef OLED_ENABLE
